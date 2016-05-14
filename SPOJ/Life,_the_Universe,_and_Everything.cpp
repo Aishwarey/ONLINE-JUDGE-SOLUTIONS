0d2138bdca0d6537b5f85a976d00007a5e29a817
Aishwarey Maheshwari <Aishwarey@users.noreply.github.com>
2016-05-14 21:39:45 +0530

    Create Life,_the_Universe,_and_Everything.cpp

---
+++ SPOJ/Life,_the_Universe,_and_Everything.cpp
@@ -0,0 +1,19 @@
+                                    /*TEST - Life, the Universe, and Everything
+                                      #basic #tutorial #ad-hoc-1*/
+  #include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+	while(1)
+	{
+		cin>>n;
+		if(n==42)
+			return 0;
+		else
+			cout<<n<<endl;
+	}
+ 
+	return 0;
+}
+ 
