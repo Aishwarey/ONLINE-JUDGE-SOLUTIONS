46cc56fa822f59662ba9245862867c39e5355505
Aishwarey Maheshwari <Aishwarey@users.noreply.github.com>
2018-10-02 12:53:03 +0530

    Update AE00.py

--- SPOJ -PYTHON/AE00.py
+++ SPOJ -PYTHON/AE00.py
@@ -1,3 +1,7 @@
+'''
+the program finds the number of rectangles that can be made using a square of length 1.
+in line 13, (inpt+1) is used because we have started the iteration from 1 and we need the iteration to include the certain value not exclude it.
+'''
 j= k= i=1
 fakt = 0
 inp = raw_input("enter the number")
@@ -9,6 +13,5 @@ if inp == 1:
 for i in range(1,inpt+1):
     for j in range(i,inp+1):
         if(i*j<= inp):
-            #print(i,j)
             fakt =fakt +1
 print(fakt)
